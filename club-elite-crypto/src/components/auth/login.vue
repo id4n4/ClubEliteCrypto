01359962bf1388f648127706b5d3a879414b6e74
id4n4 <nagatiitoo@hotmail.com>
2022-08-12 15:16:32 -0500

    login config with route

---
+++ club-elite-crypto/src/components/auth/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form>
+            <input type="text" placeholder="Usuario">
+            <input type="password" placeholder="contraseña">
+            <input type="submit" value="Acceso">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div, form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    form{
+        height: 20vh;
+        justify-content: space-around;
+    }
+</style>
--- club-elite-crypto/src/router/index.js
+++ club-elite-crypto/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/auth/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: login
+    },
     {
       path: '/',
       name: 'HelloWorld',
